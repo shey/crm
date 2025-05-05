710d2c7261470b881f6b3a7f910b263320e4e08e
shey <shey@shey.ca>
2025-05-04 21:21:44 -0400

    post and patch work!

--- test/requests/contacts_test.rb
+++ test/requests/contacts_test.rb
@@ -23,4 +23,45 @@ class ContactsTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_match "data", @response.body
   end
+
+  test "DELETE contact/id delets a contact" do
+    contact = Contact.create!(
+      name: "Delete Me",
+      email: "delete@example.com",
+      tags: ["hi"]
+    )
+
+    assert_difference "Contact.count", -1 do
+      delete contact_url(contact), as: :json
+    end
+
+    assert_response :no_content
+  end
+
+  test "POST contact/ creates a contact" do
+    assert_difference "Contact.count", 1 do
+      post contacts_url, params: {
+        contact: {
+          name: "New",
+          email: "new@example.com",
+          tags: ["test", "urgent"]
+        }
+      }, as: :json
+    end
+
+    assert_response :created
+    assert_match "new@example.com", @response.body
+  end
+
+  test "PATCH /contacts/id updates a contact" do
+    contact = Contact.first
+    patch contact_url(contact), params: {
+      contact: {
+        tags: contact.tags + ["zzzzz"]
+      }
+    }, as: :json
+
+    assert_response :success
+    assert_includes Contact.find(contact.id).tags, "zzzzz"
+  end
 end
