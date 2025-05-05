8a910a4948dcb4ed6145abe1095cfeac918e1231
shey <shey@shey.ca>
2025-05-04 21:17:31 -0400

    show works on the contact controller

---
+++ test/requests/contacts_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class ContactsTest < ActionDispatch::IntegrationTest
+  test "GET /contacts returns contacts" do
+    get contacts_url, as: :json
+
+    assert_response :success
+    assert_match "data", @response.body
+  end
+
+  test "GET /contacts/id returns a contact" do
+    contact = Contact.first
+    get contact_url(contact), as: :json
+
+    assert_response :success
+    assert_includes @response.body, contact.email
+  end
+end
