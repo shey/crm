7e5f4f84daf8729f5d9674f87a4e32e545d0fd12
shey <shey@shey.ca>
2025-05-04 20:52:59 -0400

    skeleton controller, curl localhost:3000/contacts/ works

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,18 @@
+class ContactsController < ApplicationController
+  before_action :set_contact, only: [:show, :update, :destroy]
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
