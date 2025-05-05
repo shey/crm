204c1d030ce08df33b77a30722a06b4724fa4d5b
Sheheryar Sewani <shey@shey.ca>
2025-05-04 20:36:05 -0400

    Contact Tagging (#4)

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,30 @@
 class Contact < ApplicationRecord
   ##################################################
+  ## Scope
+  ##################################################
+  scope :with_tag, ->(tag) {
+    where("tags @> ARRAY[?]::varchar[]", [tag.to_s.downcase])
+  }
+
+  ##################################################
   ## Validations
   ##################################################
   validates :email, presence: true, uniqueness: true
+
+  ##################################################
+  ## Callbacks
+  ##################################################
+  before_save :normalize_tags
+
+  # For testing and development semi-private methods
+  # are preferred, i.e, don't use private methods
+  # please
+  def normalize_tags
+    self.tags = tags
+      .compact # remove nils
+      .map(&:strip)
+      .map(&:downcase)
+      .uniq
+      .sort
+  end
 end
--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -15,4 +15,21 @@ class ContactTest < ActiveSupport::TestCase
     assert_not duplicate.valid?
     assert_includes duplicate.errors[:email], "has already been taken"
   end
+
+  test "should normalize tags before save" do
+    contact = Contact.create!(
+      name: "Test User",
+      email: "hi@localhost.local",
+      tags: [" Lead ", "lead", "FINALIZED", nil]
+    )
+    assert_equal ["finalized", "lead"], contact.tags
+  end
+
+  test "should find contacts by tag" do
+    contacts = Contact.with_tag("finalized")
+
+    assert_equal 2, contacts.count
+    assert_includes contacts, contacts(:cathy)
+    assert_includes contacts, contacts(:andrew)
+  end
 end
