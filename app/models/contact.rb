4fb609b3aee18d0b5f3e23a4d3c5145cf66a6e6a
shey <shey@shey.ca>
2025-05-04 20:16:36 -0400

    uniqueness enforced

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ApplicationRecord
+  ##################################################
+  ## Validations
+  ##################################################
+  validates :email, presence: true, uniqueness: true
 end
